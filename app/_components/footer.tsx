1ccfbcc2a613d4f7e45bef02fbb3a8b837d73050
leopinheirosilva <leonardopinheirosilva16@gmail.com>
2025-11-13 18:43:45 -0300

    update

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,10 +1,10 @@
 const Footer = () => {
   return (
     <footer className=" bg-gray-300 p-7">
-      <p className="text-muted-foreground text-xs font-semibold">
+      <p className="text-muted-foreground text-xs font-semibold text-center">
         © 2024 Barbearia SaaS.
       </p>
-      <p className="text-xs text-muted-foreground">Todos os direitos reservados.</p>
+      <p className="text-xs text-muted-foreground text-center">Todos os direitos reservados.</p>
     </footer>
   );
 };
