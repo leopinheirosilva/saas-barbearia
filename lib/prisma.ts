f8aa2ef43f4466d9bd1bd4f83e182aa61d296e6e
leopinheirosilva <leonardopinheirosilva16@gmail.com>
2025-11-22 14:16:36 -0300

    feat: configure Prisma with PostgreSQL adapter and update schema

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,13 @@
 // lib/prisma.ts
 import { PrismaClient } from "../app/generated/prisma/client"; // Import PrismaClient from Prisma package
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaPg({ connectionString });
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }; // Extend global object to hold PrismaClient instance
 
-export const prisma = globalForPrisma.prisma || new PrismaClient(); // Use existing instance or create a new one
+export const prisma = globalForPrisma.prisma || new PrismaClient({adapter}); // Use existing instance or create a new one
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma; // Assign instance to global object in development mode
